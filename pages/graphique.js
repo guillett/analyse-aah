855c37e74dae5e45321836c831c1f8cd415a7147
Thomas Guillet <thomas@palya.fr>
2021-06-19 01:32:02 +0200

    Renouvelle l'analyse

--- pages/graphique.js
+++ pages/graphique.js
@@ -2,8 +2,73 @@ import {useState, useCallback, useEffect, Component} from 'react'
 import fetch from 'isomorphic-unfetch'
 import lib from '../lib'
 import * as d3 from "d3"
+import { ResponsiveLine } from '@nivo/line'
 
 import 'iframe-resizer/js/iframeResizer.contentWindow'
+
+const MyResponsiveLine = ({ data /* see data tab */ }) => (
+    <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'linear' }}
+        yScale={{ type: 'linear', min: 0, max: 'auto', reverse: false }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Graphique',
+            legendOffset: 36,
+            legendPosition: 'middle'
+        }}
+        axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'count',
+            legendOffset: -40,
+            legendPosition: 'middle'
+        }}
+        enablePoints={false}
+        pointSize={10}
+        lineWidth={4}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemBackground: 'rgba(0, 0, 0, .03)',
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+)
 
 function getProps() {
     const queryString = (window.document.location.search || '')
@@ -17,23 +82,45 @@ function getProps() {
     const props = Object.assign({source: 'static/full.json'}, params)
     return props
 }
-
-function fetchData(source) {
-    const prs = getProps()
-    const url = source + "?value=" + (prs.value || 0)
-    console.log(url)
-    return fetch(url)
-    .then(response => response.json())
-}
-
 const colors = d3.scaleOrdinal(d3.schemeCategory10)
 
 function Graphique () {
     const [source, setSource] = useState()
     const [payload, setPayload] = useState()
-    const [data, setData] = useState()
+    const [data, setData] = useState([])
     const [names, setNames] = useState([])
+    const [cData, setCData] = useState([])
+    const [taxData, setTaxData] = useState([])
     const [currentValues, setCurrentValues] = useState([])
+
+    const [jsonData, setJsonData] = useState('')
+
+    const [abat_forf, setAbat_forf] = useState(0)
+    const [abat_forf_enf, setAbat_forf_enf] = useState(0)
+    const [prog_abat, setProg_abat] = useState(0)
+    const [deconj, setDeconj] = useState(false)
+
+
+    function fetchData(source) {
+        const prs = getProps()
+        var items = [
+            ["aah_abattement_forfaitaire_amendement", prs.a],
+            ["aah_abattement_forfaitaire_enf", prs.b],
+            ["aah_niveau_smic_abattement_conjoint", prs.c],
+            ["aah_deconjugalise", prs.d],
+            ["aah_maj_conj", prs.e]
+        ]
+        var suffix = ""
+        items.forEach(t => {
+            if (t[1]) {
+                suffix += t[0] + "=" + t[1]
+            }
+        })
+        const url = source + "?" + suffix
+        setSource(url)
+        return fetch(url)
+        .then(response => response.json())
+    }
 
     d3.formatDefaultLocale({
         "decimal": ",",
@@ -49,12 +136,35 @@ function Graphique () {
     var width = 950
 
     useEffect(() => {
+        const r = data.map(p => {
+            return {x: p.name, y: p.total}
+        })
+        setCData([{
+            "id": "total",
+            "data": r
+        }])
+        const t = r.reduce((a, v) => {
+            const p = a[a.length - 1] || { x: 0, base: 0}
+            a.push({
+                x: v.x,
+                y: 1 - (v.y - p.base)/(v.x - p.x),
+                base: v.y
+            })
+            return a
+        }, []).slice(1)
+        setTaxData([{
+            "id": "total",
+            "data": t
+        }])
+    }, [data])
+
+    useEffect(() => {
         const {source} = getProps()
-        setSource(source)
 
         fetchData(source)
         .then(payload => {
             setPayload(payload)
+            setJsonData(JSON.stringify(payload))
 
             var rawData = payload.data
             lib.preprocess(rawData)
@@ -116,7 +226,9 @@ function Graphique () {
             svg.append("g")
               .call(yAxis);
         })
-    }, [])
+    }, [abat_forf, abat_forf_enf, prog_abat, deconj])
+
+    var to64 = typeof btoa == "undefined" ? e => e && Buffer.from(e).toString('base64') : btoa
 
     return (
         <div>
@@ -134,8 +246,19 @@ function Graphique () {
             <div><a
                 target="_blank"
                 rel="noopener"
-                href={source}>Source
-            </a></div>
+                href={source}>Lien
+            </a>{/* <a
+                download="data.json"
+                href={"data:application/json;base64,"+to64(jsonData)}>Données
+            </a>*/}</div>
+{/*
+            <div style={{height: '400px', width: '100%'}}>
+                {MyResponsiveLine({data: cData })}
+            </div>
+
+            <div style={{height: '400px', width: '100%'}}>
+                {MyResponsiveLine({data: taxData })}
+            </div>*/}
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -15,9 +15,6 @@ const layoutStyle = {
 function Index () {
   useEffect(() => {
     iFrameResize({ log: false }, '#couple_non_beneficiaire')
-    iFrameResize({ log: false }, '#conjoint_beneficiaire_aah')
-    iFrameResize({ log: false }, '#conjoint_beneficiaire_aah_avant')
-    iFrameResize({ log: false }, '#conjoint_beneficiaire_aah_apres')
     iFrameResize({ log: false }, '#couple_beneficiaire_aah_avant')
     iFrameResize({ log: false }, '#couple_beneficiaire_aah_apres')
     iFrameResize({ log: false }, '#couple_beneficiaire_aah_2enf_avant')
@@ -40,60 +37,42 @@ function Index () {
 
             <p>Avant de regarder ce qui se passe pour les bénéficiaires de l'AAH, on peut voir la dynamique pour un couple dont aucun des membres en bénéficie.</p>
 
+            <p><strong>Note&nbsp;: </strong>Cette analyse a été revue à partir des nouvelles informations partagées par le gouvernement sur l'application concrète des abattements forfaitaires.</p>
+
             <h2>Ressources pour un couple dont aucun adulte n'est bénéficiaire de l'AAH</h2>
             <iframe id="couple_non_beneficiaire" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/couple_non_beneficiaire.json`} style={iframeStyle}/>
 
             <p>Ce graphique met en évidence trois dispositifs de solidarité nationale, le RSA, la prime d'activité et l'aide au logement.</p>
             <p>On voit aussi que, globalement, les ressources vont toujours en augmentant.</p>
 
-            <h2>Ressources pour un couple dont un parent est sans salaire et bénéficiaire de l'AAH</h2>
+            <h2>Ressources pour un couple dont un adulte est sans salaire et bénéficiaire de l'AAH</h2>
             <div style={{
               display: 'flex'
             }}>
               <div style={{ width: '100%'}}>ACTUELLEMENT
-              <iframe id="conjoint_beneficiaire_aah_avant" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/conjoint_beneficiaire_aah_avant.json`} style={iframeStyle}/>
+              <iframe id="couple_beneficiaire_aah_avant" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/re/couple_avant.json`} style={iframeStyle}/>
               </div>
               <div style={{ width: '100%'}}>AVEC L'AMENDEMENT
-              <iframe id="conjoint_beneficiaire_aah_apres" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/conjoint_beneficiaire_aah_apres.json`} style={iframeStyle}/>
+              <iframe id="couple_beneficiaire_aah_apres" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/re/couple_apres.json`} style={iframeStyle}/>
               </div>
             </div>
 
-            <p>Le premier graphique met en évidence certains des problèmes actuels. À partir d'environ 1000 €, les ressources globales du couple diminuent !
-            Vous travaillez plus mais vous gagnez littéralement moins ! Cette diminution n'est compensée qu'à partir de 1 800 €.
-            </p>
+            <p>Sur ce graphique, on voit l'argument du gouvernement. l'AAH ne croît pas dès 1050 € mais 1250€. Cela dit, on voit clairement que les ressources stagnent voire diminuent lorsque le salaire est entre 1200 € et 2000 €. Il faut arriver à 2100 € pour enfin voir les ressources augmenter à nouveau.</p>
 
-            <p>L'amendement proposé par la majorité ne règle pas du tout le problème, elle le déplace ! Le premier pic n'est plus à 1 000 mais à environ 1 100 euros et cela n'est compensé qu'à partir de 2 000 € !
-            </p>
-
-            <h2>Ressources pour un couple dont les deux adultes sont bénéficiaires de l'AAH (l'un d'eux étant sans salaire)</h2>
+            <h2>Ressources pour une famille avec 2 enfants dont l'adulte bénéficiaire de l'AAH est sans salaire</h2>
 
             <div style={{
               display: 'flex'
             }}>
               <div style={{ width: '100%'}}>ACTUELLEMENT
-              <iframe id="couple_beneficiaire_aah_avant" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/couple_beneficiaire_aah_avant.json`} style={iframeStyle}/>
+              <iframe id="couple_beneficiaire_aah_2enf_avant" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/re/parents_avant.json`} style={iframeStyle}/>
               </div>
               <div style={{ width: '100%'}}>AVEC L'AMENDEMENT
-              <iframe id="couple_beneficiaire_aah_apres" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/couple_beneficiaire_aah_apres.json`} style={iframeStyle}/>
+              <iframe id="couple_beneficiaire_aah_2enf_amendement" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/re/parents_apres.json`} style={iframeStyle}/>
               </div>
             </div>
 
-            <p>Les problèmes sont encore plus accentués lorsque les deux conjoints sont bénéficiaires de l'AAH.</p>
-
-            <h2>Ressources pour une famille avec 2 enfants dont les deux adultes sont bénéficiaires de l'AAH (l'un d'eux étant sans salaire)</h2>
-
-            <div style={{
-              display: 'flex'
-            }}>
-              <div style={{ width: '100%'}}>ACTUELLEMENT
-              <iframe id="couple_beneficiaire_aah_2enf_avant" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/couple_beneficiaire_aah_2enf_avant.json`} style={iframeStyle}/>
-              </div>
-              <div style={{ width: '100%'}}>AVEC L'AMENDEMENT
-              <iframe id="couple_beneficiaire_aah_2enf_amendement" src={`${config.assetPrefix}/graphique?source=${config.assetPrefix}/static/aah/couple_beneficiaire_aah_2enf_apres.json`} style={iframeStyle}/>
-              </div>
-            </div>
-
-            <p>Et l'amendement a un impact négatif fort sur les familles, notamment avec deux enfants.</p>
+            <p>L'abattement supplémentaire pour les enfants à charge limite les pertes pour les parents, mais pour une famille de 2 enfants avec 3000 € de salaire, une reste une perte d'environ 150€ / mois. De plus, là encore, les ressources du foyer se retrouvent quasiment figées entre 2000 et 3000 € de salaire.</p>
 
 
           <p>Pour en savoir plus, contactez <a target="_blank" rel="noopener" href="https://twitter.com/1h0ma5">moi sur Twitter</a>.
